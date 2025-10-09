17ab01536eec1af80d46dcef454ea364157cfc84
andxbes <andxbes@gmail.com>
2025-10-09 17:47:29 +0300

    generic

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,1 +1,49 @@
-fn main() {}
+#[derive(Debug)]
+enum DigitalContent {
+    AudioFile,
+    VideoFile,
+}
+
+#[derive(Debug)]
+struct ChatMessage<T> {
+    content: T,
+    time: String,
+}
+
+impl ChatMessage<DigitalContent> {
+    fn consume_entertainment(&self) {
+        println!("Watching the {:?}", self.content);
+    }
+}
+
+impl<T> ChatMessage<T> {
+    fn retrieve_time(&self) -> String {
+        self.time.clone()
+    }
+}
+
+fn main() {
+    let message = ChatMessage {
+        content: "Hi, lol",
+        time: String::from("2025-03-12"),
+    };
+    println!("{:?}", message.retrieve_time());
+
+    let notification = ChatMessage {
+        content: "whats your favorite pizza topping?",
+        time: String::from("2025-04-12"),
+    };
+    println!("{:?}", notification.retrieve_time());
+
+    let audio = ChatMessage {
+        content: DigitalContent::AudioFile,
+        time: String::from("2025-05-12"),
+    };
+    println!("{:?}", audio.retrieve_time());
+
+    let video = ChatMessage {
+        content: DigitalContent::VideoFile,
+        time: String::from("2025-06-12"),
+    };
+    println!("{:?}", video.retrieve_time());
+}

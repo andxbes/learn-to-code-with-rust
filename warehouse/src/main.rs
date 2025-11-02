05c3ce450f085e1afd2627a4509b3f12a293cfad
andxbes <andxbes@gmail.com>
2025-11-02 16:13:39 +0200

    236

--- warehouse/src/main.rs
+++ warehouse/src/main.rs
@@ -5,14 +5,14 @@ mod orders;
 //     pub const MANAGER: &str = "Oliver Orderson";
 // }
 
-use crate::inventory::{FLOOR_SPACE, MANAGER, talk_to_manager};
+use crate::inventory::{FLOOR_SPACE, talk_to_manager};
 use inventory::products::{Item, ProductCategory};
 
 fn main() {
     println!(
         "Our managers are {} and {}. We have {} square feet of floor space",
         // crate::inventory::MANAGER,
-        MANAGER,
+        inventory::MANAGER,
         orders::MANAGER,
         FLOOR_SPACE
     );

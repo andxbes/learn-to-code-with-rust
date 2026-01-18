e615f9db3e1ca1512f28e1b5b707fa05fbb7937f
Andrey Bescenniy <andxbes@gmail.com>
2026-01-18 15:13:33 +0200

    249

---
+++ fitness/src/lib.rs
@@ -0,0 +1,33 @@
+mod cardio;
+pub mod diet {
+    const NUTRITIONIST: &str = "Norah Nutrition";
+
+    pub fn ask_about_program() {
+        println!("The nutritionst is {NUTRITIONIST}");
+    }
+}
+mod weightlifting;
+
+use cardio::{CardioTool, Exercise as CardioExercise};
+use weightlifting::Exercise as WeightliftingExercise;
+
+use crate::cardio::ask_about_program;
+
+#[derive(Debug)]
+pub struct GymWorkout {
+    cardio: CardioExercise,
+    weightlifting: WeightliftingExercise,
+}
+
+impl GymWorkout {
+    pub fn new() -> Self {
+        diet::ask_about_program();
+        cardio::ask_about_program();
+        weightlifting::ask_about_program();
+
+        Self {
+            cardio: CardioExercise::new(String::from("Thursday"), CardioTool::Treadmill, 30),
+            weightlifting: WeightliftingExercise::new(String::from("Bench Press"), 8),
+        }
+    }
+}
---
+++ fitness/src/main.rs
@@ -0,0 +1,5 @@
+use fitness::GymWorkout;
+fn main() {
+    let workout = GymWorkout::new();
+    println!("{:#?}", workout);
+}

5e8b55c3f1db8452e5971ca7b60cddd41cd5f313
Andrey Bescenniy <andxbes@gmail.com>
2026-01-18 21:41:08 +0200

    258

--- strings/src/bin/concatenation.rs
+++ strings/src/bin/concatenation.rs
@@ -1,10 +1,16 @@
 fn main() {
-    let first_name = String::from("Sylvester");
+    let mut first_name = String::from("Sylvester");
+
+    first_name.push(' ');
+
+    let spice = String::from(" - ");
     let last_name = String::from("Stallone");
 
-    let full_name = first_name + &last_name;
+    let full_name = first_name + &spice + &last_name;
     println!("{full_name}");
 
     // Invalid
     // println!("{first_name}");
+    // println!("{first_name}");
+    // println!("{last_name}");
 }
--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,1 +1,47 @@
-fn main() {}
+use std::io;
+
+fn make_money(text: &mut String) {
+    text.push_str("$$$");
+}
+
+fn trim_and_capitalize(text: &str) -> String {
+    return text.trim().to_uppercase();
+}
+
+fn elements(text: &str) -> Vec<&str> {
+    text.split("!").collect::<Vec<&str>>()
+}
+
+fn get_identity() -> String {
+    let mut first_name = String::new();
+    let mut last_name = String::new();
+    let input = io::stdin();
+
+    println!("What is your first name?");
+    input
+        .read_line(&mut first_name)
+        .expect("Failed to collect first name");
+    println!("What is your last name?");
+    input
+        .read_line(&mut last_name)
+        .expect("Failed to collect last name");
+
+    return format!("{} {}", first_name.trim(), last_name.trim());
+}
+
+fn main() {
+    let mut amount = String::from("40");
+    make_money(&mut amount);
+
+    println!("{amount}");
+
+    let banana = trim_and_capitalize("      banana       ");
+    println!("{banana}");
+
+    let collection = elements("Gold!Silver!Platinum");
+
+    println!("{:?}", collection);
+
+    let full_name = get_identity();
+    println!("{full_name}");
+}
